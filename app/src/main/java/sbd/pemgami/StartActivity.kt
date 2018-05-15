e80b106b8492288145f2cc9d193771f0026542d2
daniel_eichinger <d.eichinger@campus.lmu.de>
2018-05-15 17:48:05 +0200

    Firebase Integration

--- app/src/main/java/sbd/pemgami/StartActivity.kt
+++ app/src/main/java/sbd/pemgami/StartActivity.kt
@@ -2,11 +2,22 @@ package sbd.pemgami
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+// nice import of layout no need for findById anymore
+import kotlinx.android.synthetic.main.activity_start.*
+import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity : AppCompatActivity() {
+    var fbAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        if (fbAuth.currentUser == null) {
+            lbl1.text = "Hello to Unknown User!"
+        } else {
+            lbl1.text = "Hello World to known User!"
+        }
+
     }
 }
