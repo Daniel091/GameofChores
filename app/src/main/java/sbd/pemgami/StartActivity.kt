495e173d17729590a03730d0633cb8d5c76d1e7e
daniel_eichinger <d.eichinger@campus.lmu.de>
2018-05-22 15:15:47 +0200

    rough implementation of user data in and off firebase

--- app/src/main/java/sbd/pemgami/StartActivity.kt
+++ app/src/main/java/sbd/pemgami/StartActivity.kt
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_start.*
 import java.util.*
 
@@ -16,6 +18,7 @@ class StartActivity : AppCompatActivity() {
     private val TAG: String = "StartActivity"
     private val fbAuth = FirebaseAuth.getInstance()
     private val RC_SIGN_IN: Int = 123
+    private val fbDatabase = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +26,11 @@ class StartActivity : AppCompatActivity() {
 
 
         if (fbAuth.currentUser != null) {
-            // user is there proceed to next
             textView.text = fbAuth.currentUser?.email
             //TODO
         } else {
             loginUser()
         }
-
 
 
         if (fbAuth.currentUser != null) {
@@ -69,14 +70,67 @@ class StartActivity : AppCompatActivity() {
                 RC_SIGN_IN)
     }
 
+    // result from Firebase UI
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         Log.d(TAG, "ResultCode $resultCode, requestCode: $requestCode")
 
         if (resultCode == Activity.RESULT_OK && requestCode == RC_SIGN_IN) {
-            // TODO Proceed to next activity
             textView.text = fbAuth.currentUser?.email
+            checkUserInDb(fbAuth.currentUser)
         }
+
+    }
+
+    private fun checkUserInDb(user: FirebaseUser?) {
+        if (user == null) return
+
+        // check if username exists in database
+        val checkUser: Query = fbDatabase.child("users").orderByChild("uid").equalTo(user.uid)
+
+        // set up listener
+        val checkListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                Log.d(TAG, dataSnapshot.childrenCount.toString())
+                if (dataSnapshot.childrenCount > 0) {
+                    Log.d(TAG, "User exists")
+                    getUserData(user)
+                } else {
+                    // add user data
+                    addUserData(user)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, "ErrorCode ${databaseError.code}, DatabaseDetails: ${databaseError.details}")
+            }
+        }
+        checkUser.addListenerForSingleValueEvent(checkListener)
+    }
+
+    private fun getUserData(user: FirebaseUser) {
+        val getUser: Query = fbDatabase.child("users").child(user.uid)
+
+        val getListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val usr = dataSnapshot.getValue(User::class.java)
+                Log.d(TAG, "Got User: ${usr?.email}")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, "ErrorCode ${databaseError.code}, DatabaseDetails: ${databaseError.details}")
+            }
+        }
+        getUser.addListenerForSingleValueEvent(getListener)
+    }
+
+    private fun addUserData(user: FirebaseUser) {
+        val usr = User(user.email, user.uid, user.email)
+
+        // add user to firebase
+        fbDatabase.child("users").child(user.uid).setValue(usr)
+                .addOnSuccessListener { Log.d(TAG, "Upload Successful") }
     }
 
 }
---
+++ app/src/main/java/sbd/pemgami/User.kt
@@ -0,0 +1,3 @@
+package sbd.pemgami
+
+class User(val usrname: String? = "", val uid: String = "", val email: String? = "")
