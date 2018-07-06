0e93926321aae07fd5fe59e7debaba9bdae2ee6b
Benedikt Strodl <benediktstrodl@Benedikts-MBP.fritz.box>
2018-07-06 19:46:23 +0200

    made gambling fragment... well prettier is the wrong word..

--- app/src/main/java/sbd/pemgami/Gambling/GamblingFragment.java
+++ app/src/main/java/sbd/pemgami/Gambling/GamblingFragment.java
@@ -1,17 +1,25 @@
 package sbd.pemgami.Gambling;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import sbd.pemgami.R;
 import sbd.pemgami.SharedPrefsUtils;
+import sbd.pemgami.User;
 
 public class GamblingFragment extends Fragment {
     private static final String TAG = "GamblingFragment";
@@ -26,16 +34,59 @@ public class GamblingFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_gambling, container, false);
         Button newGame = view.findViewById(R.id.gamb_startGame);
+        TextView gambleText = view.findViewById(R.id.gamb_gambleText);
+        TextView gambleText2 = view.findViewById(R.id.gamb_gambleText2);
+        final TextView ruleheader = view.findViewById(R.id.gamb_ruleheader);
+        final TextView rules = view.findViewById(R.id.gamb_rulebook);
+        final LottieAnimationView sadface = view.findViewById(R.id.gamb_sadface);
+        final TextView nopoints = view.findViewById(R.id.gamb_nopoints);
 
 
-        newGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: Button is clicked");
-                Intent intent = new Intent(getActivity(), GamblingActivity.class);
-                startActivity(intent);
-            }
-        });
+        User user = SharedPrefsUtils.readLastUserFromSharedPref(getContext());
+
+        //let it blink
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(50); //You can manage the time of the blink with this parameter
+        anim.setStartOffset(30);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        gambleText.startAnimation(anim);
+
+        Animation anim2 = new AlphaAnimation(0.0f, 1.0f);
+        anim2.setDuration(30);
+        anim2.setStartOffset(9);
+        anim2.setRepeatMode(Animation.REVERSE);
+        anim2.setRepeatCount(Animation.INFINITE);
+        gambleText2.startAnimation(anim2);
+
+        Log.d(TAG, "onCreateView: User Points:" + user.getPoints());
+
+        if (user.getPoints() == 0) {
+            newGame.setTextColor(Color.GRAY);
+
+            newGame.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ruleheader.setVisibility(View.GONE);
+                    rules.setVisibility(View.GONE);
+                    sadface.setVisibility(View.VISIBLE);
+                    sadface.animate();
+                    nopoints.setVisibility(View.VISIBLE);
+                }
+            });
+
+        } else {
+
+            newGame.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick: Button is clicked");
+                    Intent intent = new Intent(getActivity(), GamblingActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
 
         Activity activity = getActivity();
         if (activity != null) {
